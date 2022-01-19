520d3cb03a188844623466733c3c45815db82637
ivanchikap <anton.ivanchik@gmail.com>
2022-01-19 10:54:22 +0200

    start from mobile-menu

--- src/js/init.js
+++ src/js/init.js
@@ -102,4 +102,53 @@ $(document).ready(function(){
             }
         ]
     });
+
+
+    var carousel = function() {
+        $('.home-slider').owlCarousel({
+            loop:true,
+            autoplay: true,
+            margin:0,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            nav:false,
+            autoplayHoverPause: false,
+            items: 1,
+            navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        });
+        $('.carousel-testimony').owlCarousel({
+            autoplay: true,
+            center: true,
+            loop: true,
+            items:1,
+            margin: 30,
+            stagePadding: 0,
+            nav: false,
+            navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+            responsive:{
+                0:{
+                    items: 1
+                },
+                600:{
+                    items: 2
+                },
+                1000:{
+                    items: 3
+                }
+            }
+        });
+
+    };
+    carousel();
 });
