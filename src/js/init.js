ce10406d4073c261a5bd31d33faffe2dcd25d75b
ivanchikap <anton.ivanchik@gmail.com>
2022-01-21 17:23:58 +0200

    header__wrapper done

--- src/js/init.js
+++ src/js/init.js
@@ -151,4 +151,39 @@ $(document).ready(function(){
 
     };
     carousel();
+
+
+    if (document.getElementById('nav__menu-btn')) {
+        let navMenuBtn = document.getElementById('nav__menu-btn');
+        let closeMenu = document.getElementById('closeMenu');
+        let header = document.getElementById('header');
+
+        navMenuBtn.addEventListener("click", function() {
+            let menu = document.getElementById('menu');
+            setTimeout(function () {
+                menu.classList.toggle('nav__mobile--active');
+            }, 100)
+        });
+        header.addEventListener("click", function(e) {
+            let target = e.target;
+            let menu = document.getElementById('menu');
+            let overlay = document.getElementById('overlay');
+
+            if (target === closeMenu || target === overlay) {
+                setTimeout(function () {
+                    menu.classList.toggle('nav__mobile--active');
+                }, 100);
+            }
+        });
+    }
+
+
+    // let menuBtn = document.getElementById('nav__btn');
+    //
+    // menuBtn.addEventListener("click", function() {
+    //     let menu = document.getElementById('nav__nav');
+    //     setTimeout(function () {
+    //         menu.classList.toggle('d-sm-none');
+    //     }, 100)
+    // });
 });
