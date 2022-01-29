008174cf7ff2913bfbdcc288bb65c983e6950707
ivanchikap <anton.ivanchik@gmail.com>
2022-01-29 18:39:05 +0200

    during bootsrap table

--- src/js/init.js
+++ src/js/init.js
@@ -1,40 +1,3 @@
-/*(function () {
-    // .header-nav навигация
-    let nav = $('.nav__list');
-    let btn = $('.nav__btn');
-    let close = $('.nav__close');
-    let logo =  $('.header__logo');
-
-    // .menu-btn - кнопка меню
-    $(btn).click(function () {
-        if (nav.hasClass('nav__list--active')) {
-            nav.removeClass('nav__list--active');
-            close.hide();
-        } else {
-            nav.addClass('nav__list--active');
-            btn.hide();
-            // logo.addClass('mx-sm-auto');
-            close.show();
-        }
-    });
-    $(close).click(function () {
-        nav.removeClass('nav__list--active');
-        setTimeout(function () {
-            btn.show();
-            // logo.removeClass('mx-auto');
-        }, 500);
-    });
-    $("#tabs").tabs( {
-            show: { effect: "blind", duration: 900 }
-        }
-    );
-    $("#tabs2").tabs( {
-            show: { effect: "blind", duration: 900 }
-        }
-    );
-})();*/
-
-
 
 $(document).ready(function(){
     // .header-nav навигация
@@ -102,6 +65,11 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $("#tabs2").tabs( {
+            show: { effect: "blind", duration: 900 }
+        }
+    );
 
 
     var carousel = function() {
